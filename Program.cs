f2781987b882c6b6269e7742c91bce346455846c
Taras Klimarchuk <klimarchuktaras@ukr.net>
2020-09-16 11:20:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
